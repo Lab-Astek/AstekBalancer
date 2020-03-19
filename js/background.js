772aa6732e96ebb991ef02da1e3600d2a545fc87
Alex Trefilov <47473263+Volifter@users.noreply.github.com>
2020-03-19 22:16:16 +0100

    [MOD] Minor refactoring

--- js/background.js
+++ js/background.js
@@ -19,9 +19,7 @@ chrome.runtime.onInstalled.addListener(function() {
             if (new URL(tab.url).hostname != "intra.epitech.eu")
                 return;
             chrome.tabs.executeScript(tab.id, {code: `var astekifier_data = JSON.parse('${JSON.stringify(request)}');`}, resp => {
-                chrome.tabs.executeScript(tab.id, {file: "js/main.js"}, resp => {
-
-                });
+                chrome.tabs.executeScript(tab.id, {file: "js/main.js"}, resp => {});
             });
         });
     });
