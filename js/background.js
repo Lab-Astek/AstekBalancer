cfe0cea7525704e6c9fa84ec0f283f97ba156f12
Alex Trefilov <47473263+Volifter@users.noreply.github.com>
2020-04-01 11:52:27 +0200

    [MOD] Limited declarativeContent to only show on appripriate pages

--- js/background.js
+++ js/background.js
@@ -3,7 +3,7 @@ chrome.runtime.onInstalled.addListener(function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [
                 new chrome.declarativeContent.PageStateMatcher({
-                    pageUrl: {}
+                    pageUrl: {hostEquals: "intra.epitech.eu", pathSuffix: "/rdv/"}
                 })
             ],
             actions: [
